4d54386fb1b640d971175c2fcefbd0f16cf2d3ee
Daniele Paterno <dpat221@gmail.com>
2018-02-20 17:35:41 +0100

    add toast "ciao" e aggiunto commento

--- app/src/main/java/com/project/sveglia/MainActivity.java
+++ app/src/main/java/com/project/sveglia/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(MainActivity.this,"ciao",Toast.LENGTH_LONG).show();
+        //secondo commit
     }
 }
