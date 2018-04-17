14683692390286dd6e21ff5d13ee77fa9db33fdc
Daniele Paterno <dpat221@gmail.com>
2018-04-17 19:53:33 +0200

    aggiornato classi DB_Helper eDB_Manager aggiungendo tutte le colonne del database

--- app/src/main/java/com/project/sveglia/DB_Helper.java
+++ app/src/main/java/com/project/sveglia/DB_Helper.java
@@ -18,11 +18,31 @@ public class DB_Helper extends SQLiteOpenHelper {
     //Colonne DB
     public static final String ID = "_id";
     public static final String TIME = "time";
-    public static final String DAY = "day";
+    public static final String NOME = "nome";
+    public static final String SONG = "song";
+    public static final String REPEAT = "repeat";
+    public static final String RITARDA = "ritarda";
+    public static final String TRAVEL_TO = "travel_to";
+    public static final String FROM = "from";
+    public static final String TO = "to";
+    public static final String ON_OFF = "on_off";
+    public static final String TRAVEL_BY = "travel_by";
 
     //query creazione tabella
-    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ( " + ID +
-            " INTEGER PRIMARY KEY, " + TIME + " TEXT, " + DAY + " TEXT);";
+    private static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " ( " +
+                    ID + " INTEGER PRIMARY KEY NOT NULL UNIQUE, " +
+                    TIME + " INTEGER NOT NULL, " +
+                    NOME + " TEXT, " +
+                    SONG + " INTEGER, " +
+                    REPEAT + " TEXT, " +
+                    RITARDA + " INTEGER, " +
+                    TRAVEL_TO + " INTEGER, " +
+                    FROM + " TEXT, " +
+                    TO + " TEXT, " +
+                    ON_OFF + " INTEGER, " +
+                    TRAVEL_BY + "TEXT " +
+                    ");";
 
 
 
--- app/src/main/java/com/project/sveglia/DB_Manager.java
+++ app/src/main/java/com/project/sveglia/DB_Manager.java
@@ -31,16 +31,43 @@ public class DB_Manager {
         db_helper.close();
     }
 
-    public void insert(int id, String time, String day){
+    public void insert(Long id, Long time, int suoneria, String nome, String ripetizioni, Boolean ritarda, Boolean travel_to, String From, String To, Boolean on_off, String mezzo ){
+        //boolean ritarda, travel_to, on_off
+        int ritarda_input;
+        if (ritarda){
+            ritarda_input=1;
+        }else{
+            ritarda_input=0;
+        }
+        int travel_to_input;
+        if (travel_to){
+            travel_to_input=1;
+        }else{
+            travel_to_input=0;
+        }
+        int on_off_input;
+        if (on_off){
+            on_off_input=1;
+        }else{
+            on_off_input=0;
+        }
         ContentValues contentValues = new ContentValues();
         contentValues.put(DB_Helper.ID, id);
         contentValues.put(DB_Helper.TIME, time);
-        contentValues.put(DB_Helper.DAY, day);
+        contentValues.put(DB_Helper.SONG, suoneria);
+        contentValues.put(DB_Helper.NOME, nome);
+        contentValues.put(DB_Helper.REPEAT, ripetizioni);
+        contentValues.put(DB_Helper.RITARDA, ritarda_input);
+        contentValues.put(DB_Helper.TRAVEL_TO, travel_to_input);
+        contentValues.put(DB_Helper.FROM, From);
+        contentValues.put(DB_Helper.TO, To);
+        contentValues.put(DB_Helper.ON_OFF, on_off_input);
+        contentValues.put(DB_Helper.TRAVEL_BY, mezzo);
         database.insert(DB_Helper.TABLE_NAME,null,contentValues);
     }
 
     public Cursor fetch(){
-        String columns[] = new String[] {DB_Helper.ID, DB_Helper.TIME, DB_Helper.DAY};
+        String columns[] = new String[] {DB_Helper.ID, DB_Helper.TIME, DB_Helper.SONG, DB_Helper.NOME, DB_Helper.REPEAT, DB_Helper.RITARDA, DB_Helper.TRAVEL_TO, DB_Helper.FROM, DB_Helper.TO, DB_Helper.ON_OFF, DB_Helper.TRAVEL_BY};
         Cursor cursor = database.query(DB_Helper.TABLE_NAME,columns,null,null,null,null,null);
         if (cursor != null){
             cursor.moveToFirst();
@@ -48,11 +75,38 @@ public class DB_Manager {
         return cursor;
     }
 
-    public int update(int id, String time, String day){
+    public int update(Long id, Long time, int suoneria, String nome, String ripetizioni, Boolean ritarda, Boolean travel_to, String From, String To, Boolean on_off, String mezzo ){
+        //boolean ritarda, travel_to, on_off
+        int ritarda_input;
+        if (ritarda){
+            ritarda_input=1;
+        }else{
+            ritarda_input=0;
+        }
+        int travel_to_input;
+        if (travel_to){
+            travel_to_input=1;
+        }else{
+            travel_to_input=0;
+        }
+        int on_off_input;
+        if (on_off){
+            on_off_input=1;
+        }else{
+            on_off_input=0;
+        }
         ContentValues contentValues = new ContentValues();
         contentValues.put(DB_Helper.ID, id);
         contentValues.put(DB_Helper.TIME, time);
-        contentValues.put(DB_Helper.DAY, day);
+        contentValues.put(DB_Helper.SONG, suoneria);
+        contentValues.put(DB_Helper.NOME, nome);
+        contentValues.put(DB_Helper.REPEAT, ripetizioni);
+        contentValues.put(DB_Helper.RITARDA, ritarda_input);
+        contentValues.put(DB_Helper.TRAVEL_TO, travel_to_input);
+        contentValues.put(DB_Helper.FROM, From);
+        contentValues.put(DB_Helper.TO, To);
+        contentValues.put(DB_Helper.ON_OFF, on_off_input);
+        contentValues.put(DB_Helper.TRAVEL_BY, mezzo);
         int i = database.update(DB_Helper.TABLE_NAME,contentValues,DB_Helper.ID + " = " + id,null);
         return i;
     }
