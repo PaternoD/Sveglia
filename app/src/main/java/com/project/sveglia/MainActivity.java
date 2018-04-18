92b37661f788fb5f3866af42c1da5ad57d2d3753
Daniele Paterno <dpat221@gmail.com>
2018-04-18 15:20:41 +0200

    aggiunto menù + e impostato layout lista vuota

--- app/src/main/java/com/project/sveglia/MainActivity.java
+++ app/src/main/java/com/project/sveglia/MainActivity.java
@@ -3,6 +3,8 @@ package com.project.sveglia;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,24 +14,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.lista_vuota);
 
-        Toast.makeText(MainActivity.this,"ciao",Toast.LENGTH_LONG).show();
-        //secondo commit
-        //terzo commit
-        //quarto
-        //quinto
-        //sesto
-        //settimo
 
-        Button btn_add = (Button)findViewById(R.id.btn_1);
+    }
 
-        btn_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Add_Alarm.class);
-                startActivity(intent);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.add_record) {
+
+            Intent add_mem = new Intent(this, Add_Alarm.class);
+            startActivity(add_mem);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
