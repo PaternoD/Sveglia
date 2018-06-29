41bf17fe1614ede770941b0d624a185946734307
simonerigon <simone.rigon@studenti.unitn.it>
2018-06-29 16:53:20 +0200

    modificato alcuni colori_1

---
+++ app/src/main/java/com/project/sveglia/Restart_Alarm_Class.java
@@ -0,0 +1,23 @@
+package com.project.sveglia;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by simonerigon on 27/06/18.
+ */
+
+public class Restart_Alarm_Class extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public static void restart_alarm(){
+
+
+
+    }
+}
